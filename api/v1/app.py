c2efff778cc033ace08e508a39f598995c0a39dc
Mohammednur <mamebb2023@gmail.com>
2023-10-02 21:15:40 +0300

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-""" Runs api routes """
+"""Module that runs api routes"""
+
+from os import getenv
 
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-from models import storage
 from api.v1.views import app_views
-
-from os import getenv
+from models import storage
 
 app = Flask(__name__)
 CORS(app, resources=r"/*", origins="0.0.0.0")
 
-app.regester_blueprint(app_views)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_storage(e):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
